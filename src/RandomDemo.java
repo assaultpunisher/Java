8984174b5af9df55abaff1a175c64008edadcf4c
Yugam <yugamofficial@gmail.com>
2020-04-11 15:35:56 +0530

    Java Programs

---
+++ src/RandomDemo.java
@@ -0,0 +1,14 @@
+
+/*
+Write a program to generate 5 Random nos.
+between 1 to 100, and it should not follow with decimal
+point.
+*/
+
+class RandomDemo{
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++){
+            System.out.println((int)(Math.random()*100));
+        }
+    }
+}
---
+++ src/Sum_Product_ofDigit.java
@@ -0,0 +1,33 @@
+/*
+Write a program to find SUM AND PRODUCT of
+a given Digit.
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Sum_Product_ofDigit{
+    public static void main(String[] args) throws IOException {
+        BufferedReader keyboard = new BufferedReader (new
+                InputStreamReader(System.in));
+        System.out.println("Enter Number:");
+        int num = Integer.parseInt(keyboard.readLine());
+        //taking value as command line argument.
+        int temp = num,result=0;
+        //Logic for sum of digit
+        while(temp>0){
+            result = result + temp;
+            temp--;
+        }
+        System.out.println("Sum of Digits from 1 to "+num+" is : "+result);
+        //Logic for product of digit
+        temp = num;
+        result = 1;
+        while(temp > 0){
+            result = result * temp;
+            temp--;
+        }
+        System.out.println("Product of Digits from 1 to "+num+" is : "+result);
+    }
+}
