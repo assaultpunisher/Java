075aea36986a8b58e6963c4c67f259c01eca0b26
Yugam <yugamofficial@gmail.com>
2020-04-12 16:27:22 +0530

    Java Programs

---
+++ src/Swap.java
@@ -0,0 +1,28 @@
+/*
+ Write a program to Swap the values
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Swap{
+    public static void main(String args[]) throws IOException {
+        BufferedReader keyboard = new BufferedReader (new
+                InputStreamReader(System.in));
+        System.out.println("Enter Number 1 :");
+        int num1 = Integer.parseInt(keyboard.readLine());
+        System.out.println("Enter Number 2 :");
+        int num2 = Integer.parseInt(keyboard.readLine());
+        System.out.println("\n***Before Swapping***");
+        System.out.println("Number 1 : "+num1);
+        System.out.println("Number 2 : "+num2);
+//Swap logic
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+        System.out.println("\n***After Swapping***");
+        System.out.println("Number 1 : "+num1);
+        System.out.println("Number 2 : "+num2);
+    }
+}
